51fb1686d5964908f83f4373bc4cdc580d517a4a
Tiarasekarsini <tiara.sekarsini.ft20@mail.umy.ac.id>
2023-05-13 13:42:42 +0700

    menambahkan TextGormField dan ElevatedButton di class update_kategori_barang

--- lib/view/update_kategori_barang.dart
+++ lib/view/update_kategori_barang.dart
@@ -1,16 +1,78 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import '../controller/kategori_barang_controller.dart';
+import '../model/kategori_barang_model.dart';
+import 'kategori_barang.dart';
+
 class UpdateKategoriBarang extends StatefulWidget {
-  const UpdateKategoriBarang({super.key});
+  const UpdateKategoriBarang({super.key, this.nama});
+  final String? nama;
 
   @override
   State<UpdateKategoriBarang> createState() => _UpdateKategoriBarangState();
 }
 
 class _UpdateKategoriBarangState extends State<UpdateKategoriBarang> {
+  final kategoriBarangController = KategoriBarangController();
+  int? id;
+  String? nama;
+
+  void updateKategoriBarang() async {
+    KategoriBarangModel kategoriBarang =
+        KategoriBarangModel(nama: nama!, id: id!);
+    await kategoriBarangController.updateKategoriBarang(kategoriBarang);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    var formkey = GlobalKey<FormState>();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Update Kategori Barang'),
+      ),
+      body: Form(
+        key: formkey,
+        child: ListView(
+          padding: const EdgeInsets.all(16.0),
+          children: [
+            TextFormField(
+              decoration: const InputDecoration(
+                hintText: 'Nama Kategori Barang',
+                labelText: 'Nama Kategori Barang',
+              ),
+              onChanged: (value) {
+                nama = nama;
+              },
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Nama Kategori is required';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                if (formkey.currentState!.validate()) {
+                  formkey.currentState!.save();
+                  updateKategoriBarang();
+                  Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const KategoriBarang()));
+
+                  var snackBar =
+                      const SnackBar(content: Text('Data Berhasil Diupdate'));
+                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                }
+              },
+              child: const Text('Simpan'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
